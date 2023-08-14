cc0c36ff4a3757aecae48c17127f8fa1da9071b3
Saikat Das <saikatdas0790@gmail.com>
2023-08-14 13:01:14 +0530

    chore: update ic specific script

--- script/ic/upgrade.sh
+++ script/ic/upgrade.sh
@@ -6,5 +6,8 @@ dfx build backend --network ic
 cargo test
 
 dfx canister install backend --mode upgrade --upgrade-unchanged --network ic --argument "(record {
-    endpoint_to_initialize_with = opt \"https://amused-welcome-anemone.ngrok-free.app/receive-metrics\";
+    endpoint_to_initialize_with = opt \"https://receive-canister-metrics-and-push-to-timeseries-d-74gsa5ifla-uc.a.run.app/receive-metrics\";
 })"
+# dfx canister install backend --mode upgrade --upgrade-unchanged --network ic --argument "(record {
+#     endpoint_to_initialize_with = opt \"https://amused-welcome-anemone.ngrok-free.app/receive-metrics\";
+# })"
